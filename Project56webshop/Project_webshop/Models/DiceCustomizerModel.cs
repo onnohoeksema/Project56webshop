27a8b092485ad21978b444d606066a0a4796c5d2
Onno Hoeksema <onnohoeksema@gmail.com>
2017-11-10 16:33:36 +0100

    added Dice model

---
+++ Project56webshop/Project_webshop/Models/DiceCustomizerModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project_webshop.Models
+{
+    public class DiceCustomizerModel
+    {
+    }
+}
