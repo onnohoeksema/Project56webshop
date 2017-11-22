e09f414f60c901355fdc2963112f9c725a77e227
Onno Hoeksema <onnohoeksema@gmail.com>
2017-11-22 14:44:01 +0100

    poging nummer drie

--- Poging3/Poging3/Angular webshop/Controllers/HomeController.cs
+++ Poging3/Poging3/Angular webshop/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace ProjectAngular.Controllers
+namespace Angular_webshop.Controllers
 {
     public class HomeController : Controller
     {
--- Poging3/Poging3/Angular webshop/Startup.cs
+++ Poging3/Poging3/Angular webshop/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace ProjectAngular
+namespace Angular_webshop
 {
     public class Startup
     {
