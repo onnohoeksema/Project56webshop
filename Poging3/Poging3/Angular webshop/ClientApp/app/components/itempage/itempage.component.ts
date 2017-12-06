d1273ce252e2930f65eae482640ef908fe1c9463
Ricardo van Oldenbarneveld genaamd Witte Tullingh <r.vanoldenbarneveld@live.nl>
2017-12-06 14:02:16 +0100

    hi

--- Poging3/Poging3/Angular webshop/ClientApp/app/components/itempage/itempage.component.ts
+++ Poging3/Poging3/Angular webshop/ClientApp/app/components/itempage/itempage.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { Http } from '@angular/http';
+
 
 @Component({
-    selector: 'itempage',
-    templateUrl: './itempage.component.html'
+    selector: "itempage",
+    templateUrl: "./itempage.component.html"
 })
 
 export class ItemPageComponent {
+
+    public Sjaak = ["D4", "D6", "D8", "D10", "D12", "D20"];
     
+
 }
+
+
