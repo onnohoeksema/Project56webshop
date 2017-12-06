a0ea2f7a540eb179e424f49b9e5e6d24d0955f31
Onno Hoeksema <onnohoeksema@gmail.com>
2017-12-06 13:36:07 +0100

    fixed merge errors

--- Poging3/Poging3/Angular webshop/ClientApp/app/app.shared.module.ts
+++ Poging3/Poging3/Angular webshop/ClientApp/app/app.shared.module.ts
@@ -11,14 +11,10 @@ import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
 import { DiceCustomizerComponent } from './components/DiceCustomizer/DiceCustomizer.component';
 import { LoginPageComponent } from './components/Login/LoginPage.component';
-<<<<<<< HEAD
 import { UserDashBoardComponent } from './components/Login/UserDashBoard.component';
-
-=======
 import { ItemPageComponent } from './components/itempage/itempage.component';
 import { ShoppingCartComponent } from './components/shoppingcart/shoppingcart.component';
 import { PaymentRedirectComponent } from './components/paymentredirect/paymentredirect.component';
->>>>>>> b6fc98793baaf91bc0842780986c2d30c4c74294
 
 @NgModule({
     declarations: [
@@ -29,13 +25,10 @@ import { PaymentRedirectComponent } from './components/paymentredirect/paymentre
         DiceCustomizerComponent,
         HomeComponent,
         LoginPageComponent,
-<<<<<<< HEAD
-        UserDashBoardComponent
-=======
+        UserDashBoardComponent,
         ItemPageComponent,
         ShoppingCartComponent,
         PaymentRedirectComponent
->>>>>>> b6fc98793baaf91bc0842780986c2d30c4c74294
     ],
     imports: [
         CommonModule,
@@ -48,13 +41,10 @@ import { PaymentRedirectComponent } from './components/paymentredirect/paymentre
             { path: 'fetch-data', component: FetchDataComponent },
             { path: 'DiceCustomizer', component: DiceCustomizerComponent },
             { path: 'LoginPage', component: LoginPageComponent },
-<<<<<<< HEAD
             { path: 'UserDashBoard', component: UserDashBoardComponent },
-=======
             { path: 'itempage', component: ItemPageComponent },
             { path: 'shoppingcart', component: ShoppingCartComponent },
             { path: 'paymentredirect', component: PaymentRedirectComponent },
->>>>>>> b6fc98793baaf91bc0842780986c2d30c4c74294
             { path: '**', redirectTo: 'home' }
         ])
     ]
