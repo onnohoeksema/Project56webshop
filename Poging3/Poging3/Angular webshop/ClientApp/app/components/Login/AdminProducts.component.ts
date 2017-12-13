fc02c800e94600956c2e96c0a17d05ad7f375b7b
shane versluis <shaneversluis2971@gmail.com>
2017-12-13 15:04:07 +0100

    Added admin dashboard

---
+++ Poging3/Poging3/Angular webshop/ClientApp/app/components/Login/AdminProducts.component.ts
@@ -0,0 +1,28 @@
+﻿import { Component, Inject } from '@angular/core';
+import { Http } from '@angular/http';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+
+import { LoginPageComponent } from './LoginPage.component';
+
+const routes: Routes = [
+    { path: 'LoginPage', component: LoginPageComponent },
+];
+
+@NgModule({
+    imports:
+    [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+//Tot hier copieren als je het wil gebruiken, daarna de Import {---} veranderen naar jou adress en de Route changen.
+@Component({
+    selector: 'AdminProducts',
+    templateUrl: './AdminProducts.component.html'
+})
+
+export class AdminProductsComponent {
+}
