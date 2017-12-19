52a165c3ed10939a5cc5cd6186127123a3e312af
shane versluis <shaneversluis2971@gmail.com>
2017-12-19 10:47:17 +0100

    admin produts fixed

--- Poging3/Poging3/Angular webshop/ClientApp/app/components/Login/AdminProducts.component.ts
+++ Poging3/Poging3/Angular webshop/ClientApp/app/components/Login/AdminProducts.component.ts
@@ -1,9 +1,11 @@
-﻿import { Component, Inject } from '@angular/core';
-import { Http } from '@angular/http';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+﻿import { NgModule } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { HttpModule } from '@angular/http';
+import { Inject } from '@angular/core';
+import { Http } from '@angular/http/src/http';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LoginPageComponent } from './LoginPage.component';
@@ -25,4 +27,16 @@ const routes: Routes = [
 })
 
 export class AdminProductsComponent {
+    public products: any //this used to be string[] //= ["Test1", "Test2", "Test3"];
+    public filteredproducts: any
+
+    constructor(private http: HttpClient) { }
+
+    ngOnInit(): void {
+
+        this.http.get('/api/ItemPage/GetAll').subscribe(data => {
+        this.products = data;
+
+        }, error => console.error(error));
+    }
 }
