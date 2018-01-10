c23a64f001d25b58a44c8d8faeefec08ceaa47f2
Onno Hoeksema <onnohoeksema@gmail.com>
2018-01-10 13:02:38 +0100

    featured item now links to item details

--- Poging3/Poging3/Angular webshop/ClientApp/app/components/home/home.component.ts
+++ Poging3/Poging3/Angular webshop/ClientApp/app/components/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { isPlatformBrowser } from '@angular/common/';
+import { Inject, PLATFORM_ID } from '@angular/core';
 
 @Component({
     selector: 'home',
@@ -9,11 +11,18 @@ export class HomeComponent implements OnInit{
 
     public featureditem: any
 
-    constructor(private http: HttpClient) {}
+    constructor(@Inject(PLATFORM_ID) private platformId: string, private http: HttpClient) {}
 
     ngOnInit(): void
     {
         this. http.get('/api/ItemPage/GetRandomItem/').subscribe(data => { this.featureditem = data ;
         }, error => console.error(error));
+    }
+
+    GotoItem(chosenitem:any){
+        if(isPlatformBrowser(this.platformId)){
+            localStorage.setItem('currentItem', chosenitem);
+            location.href = "itemspecifics";
+        }
     }
 }
