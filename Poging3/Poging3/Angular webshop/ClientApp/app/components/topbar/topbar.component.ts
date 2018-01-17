9575f6ae72fc2f65226a0f0a824e0df00a2f689c
Khanh <0908532@users.noreply.github.com>
2018-01-17 10:08:09 +0100

    Small changes

---
+++ Poging3/Poging3/Angular webshop/ClientApp/app/components/topbar/topbar.component.ts
@@ -0,0 +1,9 @@
+﻿import { Inject, Component, PLATFORM_ID, OnInit } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+
+@Component({
+    selector: 'top-bar',
+    templateUrl: './topbar.component.html',
+})
+export class TopBarComponent {
+}
