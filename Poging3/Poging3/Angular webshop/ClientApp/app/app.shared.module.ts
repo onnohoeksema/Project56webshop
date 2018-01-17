4687cf43b43551883f689df82c9b4a71cb8af063
Khanh <0908532@users.noreply.github.com>
2018-01-17 12:53:54 +0100

    Revert "i wanna pull"
    This reverts commit 8928eecd8604a58c97fc839276f82601b537e705.

--- Poging3/Poging3/Angular webshop/ClientApp/app/app.shared.module.ts
+++ Poging3/Poging3/Angular webshop/ClientApp/app/app.shared.module.ts
@@ -31,7 +31,6 @@ import { ItemCategoriesComponent } from './components/itemcategories/itemcategor
 import { ItemSpecificsComponent } from './components/itemspecifics/itemspecifics.component';
 import { SearchResultsComponent } from './components/searchresults/searchresults.component';
 import { LocationComponent } from './components/location/location.component';
-import { TopBarComponent } from './components/topbar/topbar.component';
 
 import { AuthGuardAd } from './guards/auth.guard.admin';
 import { AuthGuard } from './guards/auth.guard';
@@ -66,8 +65,7 @@ import { PagerService } from './services/PagerService.service';
         ItemCategoriesComponent,
         ItemSpecificsComponent,
         SearchResultsComponent,
-        LocationComponent,
-        TopBarComponent
+        LocationComponent
     ],
     imports: [
         AgmCoreModule.forRoot({
